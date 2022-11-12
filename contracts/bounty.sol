e30e55a07e0134bfafebf6b006b8d7c29f072865
yetianwang <yetianw@mit.edu>
2022-11-12 16:15:36 -0500

    solidity contract

--- contracts/bounty.sol
+++ contracts/bounty.sol
@@ -2,12 +2,28 @@
 pragma solidity ^0.8.0;
 contract Bounty {
     struct Post {
-       string address;
+       string addr;
        string description;
        bool claimed; // false if has not been claimed
        uint256 price; // current TRX bid
    }
-     function claim(Post x) public {
-        x.claimed = True;
+   
+    function claim(Post post) public {
+        post.claimed = true;
     }
+    function get(Post post) public view returns (uint) {
+        return post.price;
+    }
+    function bid(uint x, Post post) public {
+        post.price += x;
+    }
+
+     function addPost(string memory addr, string memory description, uint256 price) public returns (bool success) {
+       Post memory post = Post(addr, description, true);
+
+
+       emit NewPost();
+
+       return true;
+   }
 }
