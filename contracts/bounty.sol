f0e21e946c786c8754acb6393534ed42fe783ee6
yetianwang <yetianw@mit.edu>
2022-11-12 17:26:38 -0500

    added package

--- contracts/bounty.sol
+++ contracts/bounty.sol
@@ -1,29 +1,42 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
+abstract contract Context {
+   function _msgSender() internal view virtual returns (address) {
+       return msg.sender;
+   }
+
+   function _msgData() internal view virtual returns (bytes calldata) {
+       this; 
+       return msg.data;
+   }
+
+   function _msgValue() internal view virtual returns (uint256 value) {
+       return msg.value;
+   }
+}
+
+
 contract Bounty {
     struct Post {
-       string addr;
-       string description;
        bool claimed; // false if has not been claimed
        uint256 price; // current TRX bid
+       address delegate;
    }
+   uint256 public postId;
+
+    mapping (uint256 => Post) public posts;
+
    
-    function claim(Post post) public {
-        post.claimed = true;
+    function claim(uint256 _postId) public {
+        require(post.claimed = false, "Bounty has already been claimed");
+
     }
-    function get(Post post) public view returns (uint) {
+    function get(Post memory post) public view returns (uint) {
         return post.price;
     }
-    function bid(uint x, Post post) public {
+    function bid(uint x, Post memory post) public {
         post.price += x;
     }
+    function addPost(uint id, )
 
-     function addPost(string memory addr, string memory description, uint256 price) public returns (bool success) {
-       Post memory post = Post(addr, description, true);
-
-
-       emit NewPost();
-
-       return true;
-   }
 }
