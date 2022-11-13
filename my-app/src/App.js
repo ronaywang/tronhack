1e5e195e23d1cc0b7ed50ea26b8a89cd231f122a
yetianwang <yetianw@mit.edu>
2022-11-12 23:01:40 -0500

    connected tron wallet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,115 @@
-import React from 'react';
+
+import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MenuBar from './components/menubar'
 import Content from './components/Content'
 
 function App() {
+
+
+  const [myMessage, setMyMessage] = useState(<h3> LOADING.. </h3>);
+  const [myDetails, setMyDetails] = useState({
+    name: 'none',
+    address: 'none',
+    balance: 0,
+    frozenBalance: 0,
+    network: 'none',
+    link: 'false',
+  });
+
+  const getBalance = async () => {
+    //if wallet installed and logged , getting TRX token balance
+    if (window.tronWeb && window.tronWeb.ready) {
+      let walletBalances = await window.tronWeb.trx.getAccount(
+        window.tronWeb.defaultAddress.base58
+      );
+      return walletBalances;
+    } else {
+      return 0;
+    }
+  };
+
+  const getWalletDetails = async () => {
+    if (window.tronWeb) {
+      //checking if wallet injected
+      if (window.tronWeb.ready) {
+        let tempBalance = await getBalance();
+        let tempFrozenBalance = 0;
+
+        if (!tempBalance.balance) {
+          tempBalance.balance = 0;
+        }
+
+        //checking if any frozen balance exists
+        if (
+          !tempBalance.frozen &&
+          !tempBalance.account_resource.frozen_balance_for_energy
+        ) {
+          tempFrozenBalance = 0;
+        } else {
+          if (
+            tempBalance.frozen &&
+            tempBalance.account_resource.frozen_balance_for_energy
+          ) {
+            tempFrozenBalance =
+              tempBalance.frozen[0].frozen_balance +
+              tempBalance.account_resource.frozen_balance_for_energy
+                .frozen_balance;
+          }
+          if (
+            tempBalance.frozen &&
+            !tempBalance.account_resource.frozen_balance_for_energy
+          ) {
+            tempFrozenBalance = tempBalance.frozen[0].frozen_balance;
+          }
+          if (
+            !tempBalance.frozen &&
+            tempBalance.account_resource.frozen_balance_for_energy
+          ) {
+            tempFrozenBalance =
+              tempBalance.account_resource.frozen_balance_for_energy
+                .frozen_balance;
+          }
+        }
+
+        //we have wallet and we are logged in
+        setMyMessage(<h3>WALLET CONNECTED</h3>);
+        setMyDetails({
+          name: window.tronWeb.defaultAddress.name,
+          address: window.tronWeb.defaultAddress.base58,
+          balance: tempBalance.balance / 1000000,
+          frozenBalance: tempFrozenBalance / 1000000,
+          network: window.tronWeb.fullNode.host,
+          link: 'true',
+        });
+      } else {
+        //we have wallet but not logged in
+        setMyMessage(<h3>WALLET DETECTED PLEASE LOGIN</h3>);
+        setMyDetails({
+          name: 'none',
+          address: 'none',
+          balance: 0,
+          frozenBalance: 0,
+          network: 'none',
+          link: 'false',
+        });
+      }
+    } else {
+      //wallet is not detected at all
+      setMyMessage(<h3>WALLET NOT DETECTED</h3>);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      getWalletDetails();
+      //wallet checking interval 2sec
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
   
   return (
     
