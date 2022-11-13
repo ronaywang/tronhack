a2641bf2bece582f0003efa51c5cbcdd85f88b19
bhoh <bhoh@mit.edu>
2022-11-12 22:15:16 -0500

    jsx stuff

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,59 +1,40 @@
 import React from 'react'
-import {Card, Box, Stack, Typography, IconButton, Divider, Chip, Switch} from '@mui/material';
+import {Card, Box, Stack, Typography, IconButton, Divider, Chip, Switch, Container} from '@mui/material';
 import './Content.css'
 
 const Content = () => {
     return (
         <div className = "content-wrapper">
-        <Card className = "job">
-            <Box sx={{ p: 2, display: 'flex' }} z-index={4}>
-                <Stack spacing={0.5}>
-                <Typography fontWeight={700}>Trash on Vassar and Main</Typography>
-                <Typography variant="body2" color="text.secondary">
-                </Typography>
-                </Stack>
-                <IconButton>
-                    Claim
-                </IconButton>
-            </Box>
-            <Divider />
-            <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-                sx={{ px: 2, py: 1, bgcolor: 'background.default' }}
-            >
-                <Chip>Active account</Chip>
-                <Switch />
-            </Stack>
-        </Card>
-
-        <Card className="job">
-            <Box sx={{ p: 2, display: 'flex' }} z-index={4}>
-                <Stack spacing={0.5}>
-                <Typography fontWeight={700}>Trash on Vassar and Main</Typography>
-                <Typography variant="body2" color="text.secondary">
-                </Typography>
-                </Stack>
-                <IconButton>
-                    Claim
-                </IconButton>
-            </Box>
-            <Divider />
-            <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-                sx={{ px: 2, py: 1, bgcolor: 'background.default' }}
-            >
-                <Chip>Active account</Chip>
-                <Switch />
-            </Stack>
-        </Card>
+        <job description={'trash'}/>
+        <job description={'notrash'}/>
      </div>
-                
-            
-        
+    )
+}
+
+const job = (description) => {
+    return (
+        <Container className="job">
+            <Box sx={{ p: 2, display: 'flex' }} z-index={2}>
+                <Stack spacing={0.5}>
+                <Typography fontWeight={700}>{description}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                </Typography>
+                </Stack>
+                <IconButton>
+                    Claim
+                </IconButton>
+            </Box>
+            <Divider />
+            <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{ px: 2, py: 1, bgcolor: 'background.default' }}
+            >
+                <Chip>Active account</Chip>
+                <Switch />
+            </Stack>
+        </Container>
     )
 }
 
